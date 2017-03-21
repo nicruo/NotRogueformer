d4f1b90400714af4d96f751a5e5fb0f4ba76b784
José Medeiros <jmedeiros@agap2.pt>
2017-03-21 19:27:00 +0000

    Some changes;

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject obj;
+
+	private Vector3 distance;
+
+	public float maxDistance = 4;
+
+	// Use this for initialization
+	void Start () {
+		distance = transform.position - obj.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if (obj.transform.position.x >= 17.6 || obj.transform.position.x <= 5)
+//			return;
+
+		Vector3 newDistance = transform.position - obj.transform.position;
+
+		transform.position = obj.transform.position + distance;
+
+		if (Mathf.Abs (newDistance.x) > maxDistance) {
+			distance = newDistance;
+
+
+		}
+	}
+}
--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -7,23 +7,41 @@ public class HeroController : MonoBehaviour {
 	private Rigidbody2D body;
 	private float horizontal;
 	private bool jump;
+	private bool grounded;
 
 	public float speed = 30;
 	public float jumpHeight = 100;
+	public float maxVerticalSpeed = 10;
 
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
 	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		grounded = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		jump = Input.GetButtonDown ("Jump");
+		jump = Input.GetButton ("Jump");
+
 		horizontal = Input.GetAxis ("Horizontal");
 	}
 
 	void FixedUpdate()
 	{
-		body.AddForce (new Vector2 (horizontal * speed, jump?jumpHeight:0 ));
+		float vertical = 0;
+		if (grounded) {
+			vertical = jump ? jumpHeight : 0;
+
+			if (jump)
+				grounded = false;
+		}
+
+		body.velocity = new Vector2 (horizontal * speed, (body.velocity.y < -maxVerticalSpeed)?(-maxVerticalSpeed):(body.velocity.y));
+
+		body.AddForce (new Vector2 (0, vertical));
 	}
 }
